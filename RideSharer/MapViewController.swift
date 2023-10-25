1824c2b942c893c66c9cc036c58a7c1f42bca739
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-10-25 00:58:21 -0400

    Created Tab Bar to access history view and map view. Added MapKit to access current location of the user.

---
+++ RideSharer/MapViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MapViewController.swift
+//  RideSharer
+//
+//  Created by Gaurav Bhambhani on 10/25/23.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        checkAuthStatus()
+        
+    }
+
+    func checkAuthStatus() {
+        
+        switch CLLocationManager.authorizationStatus() {
+            
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            
+        case .denied:
+            let deniedAlert = UIAlertController(title: "You have not shared your location!", message: "In order for your driver to get to you, we need to access your location.", preferredStyle: .alert)
+            deniedAlert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            self.present(deniedAlert, animated: true)
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+            
+        case .restricted: break
+            
+        case .authorizedAlways:
+            mapView.showsUserLocation = true
+        }
+    }
+}
